7516cd04d393ebd83c3949c15b5b9e78a826c5ab
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-23 18:07:23 -0400

    add validations and relationships in all models

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,6 +1,12 @@
 class Form < ApplicationRecord
   extend FriendlyId
+
+  belongs_to :user
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy  
+  
   friendly_id :title, use: :slugged
   
-  belongs_to :user
+  validates :title, :description, :user, presence: true
+  validates :title, uniqueness: true
 end
