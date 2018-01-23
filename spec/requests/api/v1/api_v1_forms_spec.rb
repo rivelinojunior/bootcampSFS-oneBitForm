5f855b03cbbf5a27b013e69b0693c7ed0341b899
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-23 11:29:02 -0400

    create test for POST /forms (create) in forms

--- spec/requests/api/v1/api_v1_forms_spec.rb
+++ spec/requests/api/v1/api_v1_forms_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Forms", type: :request do
+  
   describe "GET /forms" do
     context "With Invalid authentication headers" do
       it_behaves_like :deny_without_authorization, :get, "/api/v1/forms"
@@ -26,6 +27,49 @@ RSpec.describe "Api::V1::Forms", type: :request do
       it "returned Forms have right datas" do
         expect(json[0]).to eql(JSON.parse(@form1.to_json))
         expect(json[1]).to eql(JSON.parse(@form2.to_json))
+      end
+    end
+  end
+
+  describe "GET /forms/:friendly_id" do
+    before do
+      @user = create(:user)
+    end
+
+    context "When form exists" do
+      context "And is enable" do
+        before do
+          @form = create(:form, user: @user, enable: true)
+        end
+
+        it "returns 200" do
+          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+          expect_status(200)
+        end
+
+        it "returned Form with right datas" do
+          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+          expect(json).to eql(JSON.parse(@form.to_json))
+        end
+
+      end
+      
+      context "And is unable" do
+        before do
+          @form = create(:form, user: @user, enable: false)
+        end
+ 
+        it "returns 404" do
+          get "/api/v1/forms/#{FFaker::Lorem.word}", params: {id: @form.friendly_id}, headers: header_with_authentication(@user)
+          expect_status(404)
+        end
+      end      
+    end
+
+    context "When form dont exists" do
+      it "returns 404" do
+        get "/api/v1/forms/#{FFaker::Lorem.word}", params: {}, headers: header_with_authentication(@user)
+        expect_status(404)
       end
     end
   end
